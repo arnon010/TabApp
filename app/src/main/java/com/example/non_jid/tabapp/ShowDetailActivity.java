b51e7a2f0dfc8d4ad92499199b1b732f39425081
arnon010 <arnon010@hotmail.com>
2016-07-22 14:04:18 +0700

    ShowDetail Complete/not map

--- app/src/main/java/com/example/non_jid/tabapp/ShowDetailActivity.java
+++ app/src/main/java/com/example/non_jid/tabapp/ShowDetailActivity.java
@@ -2,6 +2,8 @@ package com.example.non_jid.tabapp;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,17 +14,55 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ShowDetailActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    //Explicit
     private GoogleMap mMap;
+    private String shopString, addressString, phoneString, promoteString, latString, lngString;
+    private TextView shopTextView, addressTextView, phoneTextView, promoteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_show_detail);
+
+        //Bind Widget
+        bindWidget();
+
+        //show Text
+        showText();
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }//onCreate
+
+    public void clickBackShowDetail(View view) {
+        finish();
+    }
+
+
+    private void showText() {
+
+        shopString = getIntent().getStringExtra("Shop");
+        addressString = getIntent().getStringExtra("Address");
+        phoneString = getIntent().getStringExtra("Phone");
+        promoteString = getIntent().getStringExtra("Promote");
+
+        shopTextView.setText(shopString);
+        addressTextView.setText("ที่อยู่ : " + addressString);
+        phoneTextView.setText("เบอร์โทร : " + phoneString);
+        promoteTextView.setText("โปรโมชั่น : " + promoteString);
+
+    }
+
+    private void bindWidget() {
+
+        shopTextView = (TextView) findViewById(R.id.txShop);
+        addressTextView = (TextView) findViewById(R.id.txAddress);
+        phoneTextView = (TextView) findViewById(R.id.txPhone);
+        promoteTextView = (TextView) findViewById(R.id.txPromote);
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
